39f4c29b349c7c331664982cc535c8a05611f013
auravila <auravila@yahoo.com>
2015-10-23 11:36:44 +1100

    Tidy dataset

--- CourseProject.R
+++ CourseProject.R
@@ -4,6 +4,7 @@ library(dplyr)
 # Prerequisites - Get Working Directory
       setwd("C:/OLDLaptop/DDrive/OldFiles/Desktop/Bills to Pay/Coursera/Data Scientist/Getting and Cleanning Data/Project/UCI HAR Dataset")
       dir <- getwd();
+      i   <- 1
 # -----------------------------------------------------------------------------------------------------------------------------
 # Read Train, Test Files into a Train, Test Dataset(s)
       dirT <- dir
@@ -15,14 +16,39 @@ library(dplyr)
             if (dir.exists(dirT)) setwd(dirT) else setwd(dir);
             xTrain     <- tbl_df(read.table(paste(dirT,"/X_train.txt", sep=""), stringsAsFactors = FALSE ))
             yTrain     <- tbl_df(read.fwf(paste(dirT,"/y_train.txt", sep=""), widths = 1 ))
-            subjtrain  <- tbl_df(read.fwf(paste(dirT,"/subject_train.txt", sep=""), widths = 1 ))
+            subjTrain  <- tbl_df(read.fwf(paste(dirT,"/subject_train.txt", sep=""), widths = 1 ))
+            colnames(Activities) <- c("actID","actName")
+            colnames(yTrain)     <- "Activity"
+            colnames(subjTrain)  <- "Subject"
             setwd(dir)
-            # Assumption, same cardinality, one column for each observation
-            Train <- cbind(subjtrain, yTrain, xTrain)
       ## Test Folder Reads  
             dirT <- paste(dir, "/test", sep ="");
             if (dir.exists(dirT)) setwd(dirT) else setwd(dir);
             xTest     <- tbl_df(read.table(paste(dirT,"/X_test.txt", sep=""), stringsAsFactors = FALSE ))
             yTest     <- tbl_df(read.fwf(paste(dirT,"/y_test.txt", sep=""), widths = 1 ))
-            subjtest  <- tbl_df(read.fwf(paste(dirT,"/subject_test.txt", sep=""), widths = 1 ))
-            Test <- cbind(subjtest, yTest, xTest)
+            subjTest  <- tbl_df(read.fwf(paste(dirT,"/subject_test.txt", sep=""), widths = 1 ))
+            colnames(yTest)    <- "Activity"
+            colnames(subjTest) <- "Subject"
+            ## Loop through variables to assign columnNames
+            for ( i in 1:nrow(Variables)) {
+                  #Make new column name tidy and unique by pasting the ordinal value
+                  NewColName          <- paste(paste((gsub(",","_",(gsub("-","_",(gsub("\"","",Variables$V2[i])))))),"@", sep=""),as.character(i), sep="")
+                  #print(i, NewColName)
+                  colnames(xTrain)[i] <- NewColName
+                  colnames(xTest)[i]  <- NewColName
+            }
+      ## Merge Data, Create Final Table      
+            # Assumption, same cardinality, one column for each observation, limit to mean or std columns and identify the dataset by adding a new col.
+            Train <- mutate(cbind(subjTrain, yTrain, select (xTrain, contains ("mean") , contains ("std"), -contains("freq"), -contains("angle") )), dataset = "Train")
+            Test  <- mutate(cbind(subjTest, yTest,   select (xTest,  contains ("mean") , contains ("std"), -contains("freq"), -contains("angle") )), dataset = "Test")
+            Final <- rbind(Train, Test)
+            # Get the Activities Names
+            Final <- inner_join (Final, Activities, by = c("Activity" ="actID"))
+            # Clean-up
+            rm(list=ls(pattern = "Train"))
+            rm(list=ls(pattern = "Test"))
+      ## Generation of tidy dataset (Group by intereset value and summarize), choose sample acceleration variables time signals "t" for project work      
+            tidy <- summarize_each(group_by(Final, Subject, actName, dataset), funs(mean))
+            tidy <- select (tidy, Subject, actName, dataset, contains("tBodyAcc_"))
+            write.table(tidy, row.name = FALSE)
+            
